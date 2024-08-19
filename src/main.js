b9e94680aa235f32205ef8ca868b524f77472f10
Sucret <sucret@163.com>
2024-08-19 20:42:46 +0800

    pinia统一出口

--- src/main.js
+++ src/main.js
@@ -4,13 +4,7 @@ import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 import App from './App.vue'
 
-import { createPinia } from 'pinia'
-const pinia = createPinia()
-
-// 引入持久化插件并使用
-import 'ant-design-vue/dist/reset.css'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-pinia.use(piniaPluginPersistedstate)
+import pinia from './store'
 
 // 暂时在这里注册路由，后期会做调整
 import menu from  './store/menu'
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+import { useAppStore } from './module/app'
+
+export { useAppStore }
+ 
+export default pinia
